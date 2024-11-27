964e0c7a5318bac395fb53f80bcf6460916c4a1e
SAHIL SINGH <sahilsingh18012002@gmail.com>
2024-11-27 19:02:20 +0530

    Create onepiece.sql

---
+++ onepiece.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE ONEPIECE;
+USE ONEPIECE;
+
+CREATE TABLE CHARACTERS (
+    name VARCHAR(25) PRIMARY KEY,                  
+    age INT NOT NULL,                           
+    role VARCHAR(25)   
+);
+
+INSERT INTO CHARACTERS
+(name, age , role)
+VALUES 
+("LUFFY" , 19 , "CAPTAIN"),
+("ZORO" , 20 , "VICE CAP" );
+
+SELECT * FROM CHARACTERS
+
